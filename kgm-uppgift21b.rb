0964aaca8ccd51513ab227fda8e33db0a028fb2c
Kristian G. Madsen <kgm@kgm.cc>
2013-02-09 12:22:20 +0100

    add uppgift21b & test files

---
+++ kgm-uppgift21b.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+# 1. vi vet att vi har 1000 ägg, 6 ägg per låda
+total_amount = 1000
+in_box = 6
+
+# 2. beräkna antal fulla lådor
+no_of_boxes = total_amount / in_box
+
+# 3. skriv ut antalet, skall bli 166 enl. uppgiften
+puts no_of_boxes
