c3b69fbee820456aa51aa25b1300448518e31af7
Kristian G. Madsen <kgm@kgm.cc>
2013-02-09 14:44:07 +0100

    handle variable number of arguments to fill method

--- kgm-uppgift21d.rb
+++ kgm-uppgift21d.rb
@@ -28,11 +28,19 @@ class Boxmachine21d
 		@box_size = box_size
 	end
 
-	def fill
+	def fill( *specificed_no_of_boxes )
+
+		# if we got an argument, only fill so many boxes
+		specificed_no_of_boxes.each do |n|
+			if n then
+				@storage -= n*@box_size
+				return n #also ignores any extra arguments
+			end			
+		end
+
+		# else, fill as many boxes as we can.
 		no_of_filled_boxes = @storage/@box_size
-
 		@storage -= no_of_filled_boxes*@box_size
-
 		return no_of_filled_boxes
 	end
 
