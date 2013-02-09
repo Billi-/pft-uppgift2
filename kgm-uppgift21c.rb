f6e91bbf60e1cf30d205efb463a30a4173d1cb11
Kristian G. Madsen <kgm@kgm.cc>
2013-02-09 13:18:48 +0100

    test suite doesn't recognise Boxmachine, fix this first

---
+++ kgm-uppgift21c.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+=begin
+# Running tests:
+
+.E
+
+Finished tests in 0.000961s, 2081.1655 tests/s, 1040.5827 assertions/s.
+
+  1) Error:
+test_new_boxmachine(Uppgift21cTest):
+NameError: uninitialized constant Uppgift21cTest::Boxmachine
+    kgm-uppgift21-test.rb:37:in `test_new_boxmachine'
+
+2 tests, 1 assertions, 0 failures, 1 errors, 0 skips
+=end
+
