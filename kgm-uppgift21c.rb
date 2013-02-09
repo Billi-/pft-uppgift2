35d2424c889a080e5524c948d226fce8b75e1c55
Kristian G. Madsen <kgm@kgm.cc>
2013-02-09 13:20:23 +0100

    test suite complains of missing initialize function

--- kgm-uppgift21c.rb
+++ kgm-uppgift21c.rb
@@ -5,13 +5,19 @@
 
 .E
 
-Finished tests in 0.000961s, 2081.1655 tests/s, 1040.5827 assertions/s.
+Finished tests in 0.000897s, 2229.6544 tests/s, 1114.8272 assertions/s.
 
   1) Error:
 test_new_boxmachine(Uppgift21cTest):
-NameError: uninitialized constant Uppgift21cTest::Boxmachine
+ArgumentError: wrong number of arguments(2 for 0)
+    kgm-uppgift21-test.rb:37:in `initialize'
+    kgm-uppgift21-test.rb:37:in `new'
     kgm-uppgift21-test.rb:37:in `test_new_boxmachine'
 
 2 tests, 1 assertions, 0 failures, 1 errors, 0 skips
+
 =end
 
+class Boxmachine
+
+end
