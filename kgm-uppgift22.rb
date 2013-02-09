354ba56efe3302a521e44cf02e68fabcb0f4cf3c
Kristian G. Madsen <kgm@kgm.cc>
2013-02-09 15:40:06 +0100

    add uppgift22 files

---
+++ kgm-uppgift22.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+# uppgift22
+
+# läs in lista av ord från fil
+words = File.open('words.txt').readlines
+words.map! { |w| w.chomp }
+puts words.inspect #debug
+
+# slumpa fram det rätta ordet från listan
+
+# kör spel-loopen:
+
+ # skriv ut hängda kroppsdelar
+ # skriv ut gissade bokstäver
+ # skriv ut ordet med icke gissade bokstäver som #
+ # läs in gissning
+
+ # om gissningen var fel, tala om det för spelaren
+ # om spelaren är hängd, gå ur spel-loopen
+ # om spelaren har gissat ordet, gå ur spel-loopen
+
+# skriv ut det rätta ordet, samt antal gissningar
