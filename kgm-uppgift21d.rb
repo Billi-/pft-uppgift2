c273e87b2487960e6e3d7bb27aec5a9434ab9bd8
Kristian G. Madsen <kgm@kgm.cc>
2013-02-09 14:49:09 +0100

    implemented fill returning nil if unable to comply

--- kgm-uppgift21d.rb
+++ kgm-uppgift21d.rb
@@ -1,18 +1,6 @@
 # encoding: utf-8
 
 =begin
-# Running tests:
-
-...............F.
-
-Finished tests in 0.004210s, 4038.0048 tests/s, 4988.1235 assertions/s.
-
-  1) Failure:
-test_fill_specified_number_of_boxes_with_insufficient_storage(Uppgift21dTest) [kgm-uppgift21-test.rb:164]:
-<nil> expected but was
-<2>.
-
-17 tests, 21 assertions, 1 failures, 0 errors, 0 skips
 =end
 
 class Boxmachine21d
@@ -31,8 +19,12 @@ class Boxmachine21d
 		# if we got an argument, only fill so many boxes
 		specificed_no_of_boxes.each do |n|
 			if n then
-				@storage -= n*@box_size
-				return n #also ignores any extra arguments
+				if @storage >= n*box_size then
+					@storage -= n*@box_size
+					return n #also ignores any extra arguments
+				else
+					return nil
+				end
 			end			
 		end
 
